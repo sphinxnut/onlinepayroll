016e4db4602fbc43356d3b9307ea617098c339a2
Gerald Tagle <78480062+GcTags@users.noreply.github.com>
2022-05-28 13:13:32 +0800

    Update apsystem.sql

--- db/apsystem.sql
+++ db/apsystem.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 27, 2022 at 06:39 AM
+-- Generation Time: May 28, 2022 at 06:16 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.29
 
@@ -59,18 +59,20 @@ CREATE TABLE `attendance` (
   `status` int(1) NOT NULL,
   `break_in` time NOT NULL,
   `time_out` time NOT NULL,
-  `num_hr` double NOT NULL
+  `num_hr` double NOT NULL,
+  `ot_hr` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `attendance`
 --
 
-INSERT INTO `attendance` (`id`, `employee_id`, `date`, `time_in`, `break_out`, `status`, `break_in`, `time_out`, `num_hr`) VALUES
-(112, 1, '2022-05-02', '09:49:17', '11:55:34', 0, '13:55:38', '20:56:55', 10.116666666667),
-(113, 1, '2022-05-03', '09:07:34', '11:07:34', 0, '13:07:34', '21:08:40', 11.016666666667),
-(114, 1, '2022-05-25', '09:11:34', '11:11:34', 0, '14:11:34', '21:12:41', 11.016666666667),
-(115, 1, '2022-05-26', '09:13:56', '11:14:02', 0, '14:14:08', '21:14:51', 11);
+INSERT INTO `attendance` (`id`, `employee_id`, `date`, `time_in`, `break_out`, `status`, `break_in`, `time_out`, `num_hr`, `ot_hr`) VALUES
+(113, 1, '2022-05-03', '09:07:34', '11:07:34', 0, '13:07:34', '21:08:40', 11.016666666667, 0),
+(114, 1, '2022-05-25', '09:11:34', '11:11:34', 0, '14:11:34', '21:12:41', 11.016666666667, 0),
+(115, 1, '2022-05-26', '09:13:56', '11:14:02', 0, '14:14:08', '21:14:51', 11, 0),
+(116, 1, '2022-05-27', '07:08:00', '11:08:37', 1, '12:08:37', '22:10:38', 8, 0),
+(118, 1, '2022-05-28', '01:14:58', '05:16:00', 0, '06:16:00', '12:14:46', 9.9833333333333, 1.9833333333333);
 
 -- --------------------------------------------------------
 
@@ -145,12 +147,10 @@ CREATE TABLE `employees` (
 --
 
 INSERT INTO `employees` (`id`, `employee_id`, `firstname`, `lastname`, `address`, `birthdate`, `contact_info`, `gender`, `position_id`, `schedule_id`, `schedule_updated_on`, `photo`, `created_on`, `updated_on`, `SSS`, `Philhealth`, `Pagibig`) VALUES
-(1, 'ABC123456789', 'Mikaela', 'Bermoy', 'Surigao City', '2018-04-02', '09079373999', 'Male', 1, 2, '2022-05-21', 'profile youtube1.jpg', '2018-04-28', '2022-05-27 12:38:16', 220.00, 420.00, 210.00),
+(1, 'ABC123456789', 'Mikaela', 'Bermoy', 'Surigao City', '2018-04-02', '09079373999', 'Male', 1, 2, '2022-05-21', 'profile youtube1.jpg', '2018-04-28', '2022-05-27 21:05:14', 500.00, 420.00, 210.00),
 (3, 'DYE473869250', 'Jonah', 'Juarez', 'Surigao City', '1992-05-02', '09123456789', 'Female', 2, 2, '2022-05-01', '3.jpg', '2018-04-30', '2022-05-27 12:15:22', 0.00, 0.00, 0.00),
-(5, 'CWI760945382', 'Gerald', 'Tagle', 'Olongapo City Zambales', '1999-09-09', '09079373999', 'Female', 2, 3, '2022-05-21', '', '2022-05-21', '2022-05-21 13:22:27', 0.00, 0.00, 0.00),
-(9, 'AER608152394', 'Sample', 'Sample', 'Sample', '2022-06-01', '09164877986', 'Female', 1, 2, '2022-05-27', '6.jpg', '2022-05-27', '2022-05-27 12:08:52', 24.00, 25.00, 26.00),
-(11, 'DSX523967048', 'Sample', 'Sample', 'Sample', '2022-05-23', '09164877986', 'Male', 1, 2, '2022-05-27', '7.jpg', '2022-05-27', '2022-05-27 12:10:59', 24.00, 25.00, 26.00),
-(12, 'NXR379142586', 'Sample', 'Sample', 'Sample', '2022-06-01', '09164877986', 'Female', 1, 2, '2022-05-27', '6.png', '2022-05-27', '2022-05-27 12:11:19', 24.00, 24.00, 26.00);
+(5, 'CWI760945382', 'Gerald', 'Tagle', 'Olongapo City Zambales', '1999-09-09', '09079373999', 'Female', 2, 3, '2022-05-21', '2.png', '2022-05-21', '2022-05-27 21:02:51', 0.00, 0.00, 0.00),
+(15, 'CAN095713624', 'Sample', 'Sample', 'Sample', '2022-06-01', '09164877986', 'Male', 1, 2, '2022-05-27', 'mora-login1.png', '2022-05-27', '2022-05-27 21:26:52', 250.00, 420.00, 210.00);
 
 -- --------------------------------------------------------
 
@@ -165,6 +165,13 @@ CREATE TABLE `overtime` (
   `rate` double NOT NULL,
   `date_overtime` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `overtime`
+--
+
+INSERT INTO `overtime` (`id`, `employee_id`, `hours`, `rate`, `date_overtime`) VALUES
+(7, '1', 1.9833333333333, 90.47, '2022-05-28');
 
 -- --------------------------------------------------------
 
@@ -275,7 +282,7 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `attendance`
 --
 ALTER TABLE `attendance`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=116;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=119;
 
 --
 -- AUTO_INCREMENT for table `cashadvance`
@@ -293,13 +300,13 @@ ALTER TABLE `deductions`
 -- AUTO_INCREMENT for table `employees`
 --
 ALTER TABLE `employees`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `overtime`
 --
 ALTER TABLE `overtime`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `position`
