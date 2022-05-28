fa65fdca84a4b1b7617b1266fdd4a25235ac040c
Gerald Tagle <78480062+GcTags@users.noreply.github.com>
2022-05-28 12:22:49 +0800

    Update payroll.php

--- admin/payroll.php
+++ admin/payroll.php
@@ -96,6 +96,7 @@
                       $ex = explode(' - ', $range);
                       $from = date('Y-m-d', strtotime($ex[0]));
                       $to = date('Y-m-d', strtotime($ex[1]));
+
                     }
 
                     $sql = "SELECT *, SUM(num_hr) AS total_hr, attendance.employee_id AS empid FROM attendance LEFT JOIN employees ON employees.id=attendance.employee_id LEFT JOIN position ON position.id=employees.position_id WHERE date BETWEEN '$from' AND '$to' GROUP BY attendance.employee_id ORDER BY employees.lastname ASC, employees.firstname ASC";
