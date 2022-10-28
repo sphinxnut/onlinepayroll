8595d563a6ecf2f638a1aae17f2f47737ef7412b
ArzelJrz10 <Arzeljrz17@gmail.com>
2022-10-28 22:27:26 +0800

    Initial commit

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -2,5 +2,5 @@
     <div class="pull-right hidden-xs">
       <b>All rights reserved</b>
     </div>
-    <strong>Copyright &copy; 2020 <a href="https://web.facebook.com/cafecervezatupi">CAFE CERVEZA</a></strong>
+    <strong>Copyright &copy; 2022 <a href="https://web.facebook.com/cafecervezatupi">CAFE CERVEZA</a></strong>
 </footer>
