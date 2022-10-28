36dfeefd8fca4731f0c3e8437fb42096b7fd2e0f
ArzelJrz10 <Arzeljrz17@gmail.com>
2022-10-28 08:28:06 +0800

    Initial commit

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -2,5 +2,5 @@
     <div class="pull-right hidden-xs">
       <b>All rights reserved</b>
     </div>
-    <strong>Copyright &copy; 2020 <a href="https://www.youtube.com/channel/UCsFgC9ggwrmYR2XqEHXpbNg/">Ser Bermz</a></strong>
+    <strong>Copyright &copy; 2020 <a href="https://www.youtube.com/channel/UCsFgC9ggwrmYR2XqEHXpbNg/">CAFE CERVEZA</a></strong>
 </footer>
