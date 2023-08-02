fc2122452117cc88f0a6847433397ff8460a5c87
Arzel John Zolina <80521619+ArzelJrz10@users.noreply.github.com>
2023-08-02 21:45:36 +0800

    sangkatutak na folder

--- connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    $database= new mysqli("localhost","root","","medical_as");
-    if ($database->connect_error){
-        die("Connection failed:  ".$database->connect_error);
-    }
-
-?>
